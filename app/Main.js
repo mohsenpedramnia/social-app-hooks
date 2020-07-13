28eb3b69110085db50eef1c68a317a7bd714313e
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2020-07-13 16:23:01 +0300

    Initial commit

---
+++ app/Main.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Footer from "./components/Footer";
+
+function Main() {
+  return (
+    <>
+      <Header />
+
+      <HomeGuest />
+
+      <Footer />
+    </>
+  );
+}
+
+ReactDOM.render(<Main />, document.querySelector("#app"));
+
+if (module.hot) {
+  module.hot.accept();
+}
